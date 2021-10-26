9049bc88b653fbb71cc761d1d1935e9f459267f5
whongzhong <zhongweihong1999@outlook.com>
2021-10-26 17:17:44 +0800

    update group logging

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def main(args):
     seed.seed_everything(args.random_seed)
 
     wandb.login()
-    wandb.init(project="BART-outgen", group="DDP")
+    wandb_run = wandb.init(project="BART-outgen", group="DDP")
 
     tokenizer, special_tokens = redefine_tokenizer(args)
 
@@ -191,7 +191,7 @@ def main(args):
         mode="max"
     )
 
-    model = OutGenerModel(args, tokenizer, special_tokens)
+    model = OutGenerModel(args, tokenizer, special_tokens, wandb_run)
     #model = OutGenerModel.load_from_checkpoint(os.path.join(args.ckpt_dir, args.\
     #    test_model), args=args, tokenizer=tokenizer, special_tokens=special_tokens)
     # accelerator="ddp", 
